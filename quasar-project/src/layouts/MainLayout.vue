f9ff5deb65c4d63ef5d6de9ff0c87413aeb7839c
Ridwan Maulana <ridwan.vendor.pasti@pegadaian.co.id>
2023-07-14 09:17:52 +0700

    test2

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -20,27 +20,7 @@
     </q-header>
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <q-list class="my-2">
-        <template v-for="item in menuItems">
-          <q-item v-if="!item.children" :key="item.id" clickable>
-            <q-item-section avatar>
-              <q-icon :name="item.icon" />
-            </q-item-section>
-
-            <q-item-section>
-              {{ item.name }}
-            </q-item-section>
-          </q-item>
-
-          <q-expansion-item v-else :icon="item.icon" :label="item.name" :key="Number(item.id)">
-            <q-item v-for="childItem in item.children" :key="childItem.id" clickable>
-              <q-item-section>
-                {{ childItem.name }}
-              </q-item-section>
-            </q-item>
-          </q-expansion-item>
-        </template>
-      </q-list>
+      <main-menu></main-menu>
     </q-drawer>
 
     <q-page-container>
@@ -62,84 +42,6 @@ export default {
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
       },
-      menuItems: [
-        {
-          id: 1,
-          name: 'Home',
-          icon: 'home',
-          link: '/menu1'
-        },
-        {
-          id: 2,
-          name: 'Bulk Galeri Foto',
-          icon: 'collections',
-          link: '/menu2'
-        },
-        {
-          id: 3,
-          name: 'Infografis',
-          icon: 'analytics',
-          link: '/menu2'
-        },
-        {
-          id: 4,
-          name: 'Video',
-          icon: 'videocam',
-          link: '/menu2'
-        },
-        {
-          id: 5,
-          name: 'Kamus Kebijakan',
-          icon: 'import_contacts',
-          link: '/menu2'
-        },
-        {
-          id: 6,
-          name: 'Repository',
-          icon: 'business_center',
-          link: '/menu2'
-        },
-        {
-          id: 90,
-          name: 'Sertifikat Kepatuhan',
-          icon: 'description',
-          children: [
-            {
-              id: 4,
-              name: 'Submenu 1',
-              link: '/submenu1'
-            },
-            {
-              id: 5,
-              name: 'Submenu 2',
-              link: '/submenu2'
-            }
-          ]
-        },
-        {
-          id: 91,
-          name: 'Analisa & Dampak',
-          icon: 'tv',
-          children: [
-            {
-              id: 4,
-              name: 'Submenu 1',
-              link: '/submenu1'
-            },
-            {
-              id: 5,
-              name: 'Submenu 2',
-              link: '/submenu2'
-            }
-          ]
-        },
-        {
-          id: 7,
-          name: 'Template Kebijakan',
-          icon: 'text_snippet',
-          link: '/menu2'
-        }
-      ]
     }
   }
 }
